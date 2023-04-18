a892d6cc13531c25c6426df6a4f3010e416ab30f
aqude <aqu.de@yandex.ru>
2023-04-18 22:33:14 +0300

    заглушка

--- app/src/main/java/com/aqude/menstrualcyclecalculator/screens/HomeScreen.kt
+++ app/src/main/java/com/aqude/menstrualcyclecalculator/screens/HomeScreen.kt
@@ -1,10 +1,11 @@
 package com.aqude.menstrualcyclecalculator.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
     // Сколько дней
-
+Text(text = "Home")
 
 }
--- app/src/main/java/com/aqude/menstrualcyclecalculator/screens/InfoScreen.kt
+++ app/src/main/java/com/aqude/menstrualcyclecalculator/screens/InfoScreen.kt
@@ -1,8 +1,9 @@
 package com.aqude.menstrualcyclecalculator.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun InfoScreen() {
-
+ Text(text = "Info")
 }
