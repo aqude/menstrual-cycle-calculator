a8fcdd5978e39497d287ccc3bd5954a64b0b3293
aqude <aqu.de@yandex.ru>
2023-05-01 21:59:37 +0300

    add fun variable

--- app/src/main/java/com/aqude/menstrualcyclecalculator/modules/TopBarInfo.kt
+++ app/src/main/java/com/aqude/menstrualcyclecalculator/modules/TopBarInfo.kt
@@ -4,41 +4,44 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
-fun TopBarInfo(textTitle: String, textInfo: String) {
+fun TopBarInfo(textTitle: String, textInfo: String, ColumnHeight: Int) {
     var infoCardState by remember {
         mutableStateOf(false)
     }
     val rotateState by animateFloatAsState(targetValue = if (infoCardState) 180f else 0f)
     Column(
-        modifier = Modifier.height(150.dp), horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.height(ColumnHeight.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
             horizontalArrangement = Arrangement.Center,
@@ -97,5 +100,5 @@ fun TopBarInfo(textTitle: String, textInfo: String) {
 @Composable
 @Preview(showBackground = true)
 fun Preview() {
-    TopBarInfo("test", "ewqqqqqqqqqqqqqqqqqqqqqqqqqqq")
+    TopBarInfo("test", "ewqqqqqqqqqqqqqqqqqqqqqqqqqqq", 150)
 }
