60c9b43a647ece57c00be63dc1f9a184c1423c13
aqude <aqu.de@yandex.ru>
2023-05-01 22:10:54 +0300

    starter create

--- app/src/main/java/com/aqude/menstrualcyclecalculator/screens/HomeScreen.kt
+++ app/src/main/java/com/aqude/menstrualcyclecalculator/screens/HomeScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -21,49 +20,72 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.aqude.menstrualcyclecalculator.ViewModel.HomeViewModel
+import com.aqude.menstrualcyclecalculator.datastore.StoreDataInfoCalendar
 import com.aqude.menstrualcyclecalculator.datastore.StoreDataName
 import com.aqude.menstrualcyclecalculator.datastore.StoreDateBloodSize
+import com.aqude.menstrualcyclecalculator.modules.TopBarInfo
 
 @Composable
 fun HomeScreen() {
 // HomeViewModel : HomeViewModel
     val context = LocalContext.current
+
     val dataStoreName = StoreDataName(context = context)
     val dataStoreBlood = StoreDateBloodSize(context = context)
+    val dataStoreCalendar = StoreDataInfoCalendar(context = context)
+
     val saveBlood = dataStoreBlood.getDataBloodSize.collectAsState(initial = "")
     val saveName = dataStoreName.getDataName.collectAsState(initial = "")
-
+    val cycle = dataStoreCalendar.cycleDurationFlow.collectAsState(initial = "").value
+    val menstrual = dataStoreCalendar.menstruationDurationFlow.collectAsState(initial = "").value
+    val year = dataStoreCalendar.yearFlow.collectAsState(initial = "").value
+    val month = dataStoreCalendar.monthFlow.collectAsState(initial = "").value
+    val day = dataStoreCalendar.dayFlow.collectAsState(initial = "").value
 
     // Сколько дней
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        TopBarInfo(
+            "Информация",
+            "Полная информация о здоровье",
+            70
+        )
+
+
         Box(modifier = Modifier.fillMaxWidth()) {
             // закруглениe
-            Box(modifier = Modifier
-                .padding(vertical = 20.dp, horizontal = 20.dp)
-                .clip(RoundedCornerShape(16.dp))
-                .background(Color.LightGray)) {
+            Box(
+                modifier = Modifier
+                    .padding(vertical = 20.dp, horizontal = 20.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.LightGray)
+            ) {
                 Box(modifier = Modifier) {
                     Column(modifier = Modifier.padding(20.dp)) {
                         Row() {
-                            Text(text = "Привет, ${saveName.value} !",
+                            Text(
+                                text = "Привет, ${saveName.value} !",
                                 Modifier
-                                    .align(Alignment.CenterVertically), fontWeight = FontWeight.Bold, fontSize = 20.sp
+                                    .align(Alignment.CenterVertically),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp
                             )
                         }
                         Box(modifier = Modifier.height(5.dp))
                         Row() {
-                            Text(text = "Колличество крови: ${saveBlood.value} мл.",
+                            Text(
+                                text = "Колличество крови: ${saveBlood.value} мл.",
                                 Modifier
-                                    .align(Alignment.CenterVertically), fontWeight = FontWeight.Bold, fontSize = 20.sp
+                                    .align(Alignment.CenterVertically),
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp
                             )
                         }
                     }
-
                 }
-
             }
         }
+
+
     }
 //        Row(Modifier.fillMaxWidth()) {
 //
